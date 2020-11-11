8b7e38c868afc13f03c2c3244d024996c0b165b5
David Williams <williamsdavida@johndeere.com>
2020-11-10 18:12:27 -0600

    Finished server.js on the back end

--- take-a-hike/server.js
+++ take-a-hike/server.js
@@ -1,19 +1,45 @@
 require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
 const routes = require('./routes');
-const methodOverride = require('method-override');
 
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
-app.use(cookieParser());
-app.use(express.static('public'));
+const corsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 200 
+  }
+
+  app.use(cors(corsOptions))
+  app.use(bodyParser.json());
+
+const verifyToken = (req, res, next) => {
+    let token = req.headers['authorization'];
+    if(token){
+        token = token.substring(7)
+    } 
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+        if(err || !decodedUser){
+            return res.status(401).send(`ERROR: ${err}`);
+        }
+        req.user = decodedUser;
+
+        next();
+    })
+}
+
+app.use('/auth', routes.auth);
+app.use('/auth/verify', verifyToken, routes.auth);
 
 app.use('/trails', routes.trail);
 app.use('/profiles', routes.profile);
 app.use('/reviews', routes.review);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`${process.env.PORT}`)
