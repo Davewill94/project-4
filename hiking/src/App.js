8480ab8a493da58d2f7379fc3cdeea4eae4f67b0
David Williams <williamsdavida@johndeere.com>
2020-11-10 18:57:04 -0600

    Built out api helpers for login and verify login and functions in the app.js

--- hiking/src/App.js
+++ hiking/src/App.js
@@ -1,31 +1,64 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Link, withRouter } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Login from './components/LogIn';
 import SignUp from './components/SignUp';
 import TrailShow from './components/TrailShow';
+import React, { Component } from 'react';
+
+import { registerUser, loginUser, verifyUser, allCities } from './services/api_helper';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  handleLogin = async (e, loginData) => {
+    e.preventDefault();
+    const currentUser = await loginUser(loginData);
+    this.setState({currentUser});
+    //push to profile
+  }
+  
+  handleVerify = async () => {
+    const currentUser = await verifyUser();
+    if (currentUser) {
+      this.setState({ currentUser });
+      //push to profile
+    }
+  }
+
+  componentDidMount() {
+    this.handleVerify();
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Header /> 
-      <Route exact path='/'>
-        <HomePage />
-      </Route> 
-      <Route path="/login" >
-        <Login />
-      </Route>
-      <Route path="/signup" >
-        <SignUp />
-      </Route>
-      <Route path="/gettrail" >
-        <TrailShow />
-      </Route>
+  render() {
+    return (
+      <div className="App">
+        <Header /> 
+        <Route exact path='/'>
+          <HomePage />
+        </Route> 
+        <Route path="/login" >
+          <Login />
+        </Route>
+        <Route path="/signup" >
+          <SignUp />
+        </Route>
+        <Route path="/gettrail" >
+          <TrailShow />
+        </Route>
 
-    </div>
-  );
+      </div>
+    );
+  }
+
 }
 
-export default App;
+export default withRouter(App);
--- hiking/src/services/api_helper.js
+++ hiking/src/services/api_helper.js
@@ -4,3 +4,21 @@ const api = axios.create({
     baseURL: "http://localhost:3001"
 })
 
+//====================== Auth ==========================
+export const verifyUser = async () => {
+    const token = localStorage.getItem('authToken');
+
+    if(token) {
+        api.defaults.headers.common.authorization = `Bearer ${token}`;
+        const resp = await api.get('/auth/verify');
+        return resp.data;
+    }
+    return false;
+}
+
+export const loginUser = async (loginData) => {
+    const resp = await api.post('/auth/login', loginData);
+    localStorage.setItem('authToken', resp.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+    return resp.data.user;
+}
